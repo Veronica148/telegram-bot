a41c4a07a2cf6671d810c54aa82ac14c9d2ea820
VeronicaL <veronica_lapunka@epam.com>
2021-05-01 00:40:56 +0300

    Allure

--- allure.sh
+++ allure.sh
@@ -7,6 +7,7 @@ cd jdk-11.0.1/bin
 apt-get update
 apt-get install -y unzip
 unzip ../../allure-2.13.9.zip
-nohup allure-2.13.9/bin/allure serve ../../target/allure-results/ &
-cd ../../target/allure-report
+nohup allure-2.13.9/bin/allure serve ../../target/allure-results/  -h localhost -p 12345 &
+pwd
+cd ../../
 ls -ll
